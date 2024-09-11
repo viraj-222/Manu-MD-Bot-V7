7184b2fe12e2d03d70d2697ea320a73263e4f6d0
vrjchanuka24@gmail.com <157101752+viraj-222@users.noreply.github.com>
2024-09-11 19:16:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "", //Add Your Session id
-ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
-ALIVE_MSG : process.env.ALIVE_MSG || "Put your alive messege",
-MENU_IMG : process.env.MENU_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
+ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/e3947a665144372a172aa.jpg",
+ALIVE_MSG : process.env.ALIVE_MSG || "hey!!!",
+MENU_IMG : process.env.MENU_IMG || "https://telegra.ph/file/e3947a665144372a172aa.jpg",
 MODE : process.env.MODE || "public",    
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-AUTO_BIO: process.env.AUTO_BIO || "true",
+AUTO_BIO: process.env.AUTO_BIO || "false",
 READ_CMD: process.env.READ_CMD || "true",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 };
